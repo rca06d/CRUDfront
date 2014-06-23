44362a0f336c1105b1d2c05fec7bf59121a593ee
Rob Allsopp <rallsopp@commercialtribe.com>
2014-06-23 08:02:46 -0600

    Moved directives into separate file

--- public/js/init.js
+++ public/js/init.js
@@ -10,53 +10,11 @@ app.config(function($sceDelegateProvider) {
 	"http://localhost:3001/**"]);
 });
 
-app.directive('robMakeDialog', function() {
-    return {
-    	scope: {
-    		dialogOptions: "=robMakeDialog"
-    	},
-    	link: function(scope, element, attrs) {
+app.directive('robMakeDialog', robMakeDialog);
 
-            $(element).dialog(scope.dialogOptions);
-        }
-    };
-});
+app.directive('robOpenDialogButton', robOpenDialogButton);
 
-app.directive('robOpenDialogButton', function() {
-    return {
-        link: function(scope, element, attrs) {
-        	element.click(function() {
-        		$("#" + attrs.robOpenDialogButton).dialog( "open" );
-        	});        
-        }
-    };
-});
-
-app.directive('robPlupload', function() {
-    return {
-        link: function(scope, element, attrs) {
-        	$(element).plupload({
-				runtimes: "html5, flash, silverlight, gears, browserplus",
-				file_data_name: attrs.name,
-				url: attrs.url,
-		        max_file_size : '100mb',
-		        multipart: true,
-		        flash_swf_url : '/public/scripts/Moxie.swf',
-    			silverlight_xap_url : '/public/scripts/Moxie.xap',
-    			init: {
-	    			BeforeUpload: function(up, file) {
-		                // Called right before the upload for a given file starts, can be used to cancel it if required
-		                //console.log(file);
-		            },
-		            UploadComplete: function(up, files) {
-		                // Called when all files are either uploaded or failed
-		                scope.$parent.getAllAudioClips();
-		            }
-		        }
-			});
-        }
-    };
-});
+app.directive('robPlupload', robPlupload);
 
 VideoAppController.$inject = ["$scope", "$http"];
 
